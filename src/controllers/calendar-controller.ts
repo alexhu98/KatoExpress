8fab4c4440842ba5793871989ca7023bb39b8a4f
Alex Hu <alexhu98@gmail.com>
2020-10-17 17:32:13 -0700

    Updated OAuth client

--- src/controllers/calendar-controller.ts
+++ src/controllers/calendar-controller.ts
@@ -5,9 +5,9 @@ import { addWeeks, formatISO, startOfToday } from 'date-fns'
 
 const { OAuth2 } = google.auth
 
-const CLIENT_ID = '872194152518-m9dmuf0i9heef3au1ld811shsn9bp0k8.apps.googleusercontent.com'
-const CLIENT_SECRET = '5mc_f_24had5LNIxT1m-CrZz'
-const REFRESH_TOKEN = '1//04jXCPLyX8sDLCgYIARAAGAQSNwF-L9Ir2rAfY_yjqBFNPN4LH4Pa3flJVI2s3MhfaxawIPUiwUPXKGSKlASMhvR8HVadLmzRrFA'
+const CLIENT_ID = '872194152518-3uo269q5lraicu050a8rkc38h870mdac.apps.googleusercontent.com'
+const CLIENT_SECRET = 'l_kFCBOCg-HhInFg-fhxmTOk'
+const REFRESH_TOKEN = '1//04ec7EkXtkTtTCgYIARAAGAQSNwF-L9IrMpCliw1p5N65PH9GXts4mg5h-tT6kNBjmc9lqhtF-10p1NrCVp5yWqo1aRyy4ooqU3E'
 
 const oAuth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET)
 oAuth2Client.setCredentials({
@@ -19,18 +19,24 @@ const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
 export const getEvents = async (req: Request, res: Response) => {
   const eventStartTime = formatISO(startOfToday())
   const eventEndTime = formatISO(addWeeks(startOfToday(), 2))
-  const result = await calendar.events.list({
-    calendarId: 'primary',
-    singleEvents: true,
-    timeMin: eventStartTime,
-    timeMax: eventEndTime,
-    orderBy: 'startTime',
-  })
-  const events = R.map((item: any) => ({
-    id: item.id,
-    summary: item.summary,
-    start: item.start.date || item.start.dateTime,
-    end: item.end.date || item.end.dateTime,
-  }), R.defaultTo([], result.data.items))
-  res.send(events)
+  try {
+    const result = await calendar.events.list({
+      calendarId: 'primary',
+      singleEvents: true,
+      timeMin: eventStartTime,
+      timeMax: eventEndTime,
+      orderBy: 'startTime',
+    })
+    const events = R.map((item: any) => ({
+      id: item.id,
+      summary: item.summary,
+      start: item.start.date || item.start.dateTime,
+      end: item.end.date || item.end.dateTime,
+    }), R.defaultTo([], result.data.items))
+    res.send(events)
+  }
+  catch (ex) {
+    console.error(`CalendarController -> getEvents -> ex`, ex)
+    res.send([])
+  }
 }
